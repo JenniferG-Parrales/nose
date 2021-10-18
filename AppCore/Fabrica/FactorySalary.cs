61b84d0517b40bc5206bfc0c69ded1eb01c66d2b
JenniferG-Parrales <85895809+JenniferG-Parrales@users.noreply.github.com>
2021-10-18 12:57:53 -0600

    Add files via upload

---
+++ AppCore/Fabrica/FactorySalary.cs
@@ -0,0 +1,32 @@
+﻿using AppCore.Processses.Payroll;
+using Domain.Entities.Empleados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCore.Factories
+{
+    public static class SalaryCalcultorFactory
+    {
+        public static ISalaryC CreateInstance(Empleado e)
+        {
+            if (e == null)
+            {
+                throw new ArgumentNullException(nameof(e));
+            }
+           
+            if (e is Docente)
+            {
+                return new DocenteSalaryCalculator();
+            }
+            else if (e is Administrativo)
+            {
+                return new AdmiSalaryC();
+            }
+            else
+            {
+                throw new Exception(nameof(e));
+            }
+        }
+    }
+}
---
+++ ProductosApp/Formularios/FrmProducto.cs
@@ -0,0 +1,52 @@
+﻿using AppCore.Interfaces;
+using Domain.Entities;
+using Domain.Enums;
+using Infraestructure.Productos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductosApp.Formularios
+{    
+    public partial class FrmProducto : Form
+    {
+        private IProductoService productoService;
+        public FrmProducto(IProductoService productoService)
+        {
+            this.productoService = productoService;
+            InitializeComponent();
+        }
+
+        private void FrmProducto_Load(object sender, EventArgs e)
+        {
+            cmbMeasureUnit.Items.AddRange(Enum.GetValues(typeof(UnidadMedida))
+                                              .Cast<object>()
+                                              .ToArray()
+                                          );
+        }
+
+        private void BtnOk_Click(object sender, EventArgs e)
+        {
+            Producto p = new Producto()
+            {
+                Id = productoService.GetLastProductoId() + 1,
+                Nombre = txtNombre.Text,
+                Descripcion = txtDesc.Text,
+                Existencia = (int)nudExist.Value,
+                Precio = nudPrice.Value,
+                FechaVencimiento = dtpCaducity.Value,
+                UnidadMedida = (UnidadMedida)cmbMeasureUnit.SelectedIndex
+            };
+
+            productoService.Create(p);
+
+            Dispose();
+        }
+    }
+}
